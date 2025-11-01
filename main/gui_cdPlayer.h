63f1ebc90d598ae899147ecbb35bfcd40c93de50
lichenyi938-dot <lichenyi938@gmail.com>
2025-11-02 05:52:55 +0800

    Update gui_cdPlayer.h

--- main/gui_cdPlayer.h
+++ main/gui_cdPlayer.h
@@ -1,35 +1,46 @@
-﻿#ifndef __GUI_CD_PLAYER_H_
-#define __GUI_CD_PLAYER_H_
+#ifndef GUI_CDPLAYER_H_
+#define GUI_CDPLAYER_H_
 
-﻿#ifndef  cplusplus
-exten "c"{
+#ifdef __cplusplus
+extern "C" {
 #endif
 
-#ifndef __"lvgl.h"
-  #ifndef __<stdbool.h>
-  #ifndef __<stdint.h>
-  #ifndef __"cdplay.h"  //提供 hmsf t等类型
-extern lv_obj_t *area topBar；
-extern lv_chart_series_t *area bottomBar
-extern lv_obj_t *area player
-extern lv_obj_t *area left
-extern lv_obj_t *area*ser_right;
+#include "lvgl.h"        // ✅ 必须放在最前，提供 lv_obj_t / lv_chart_series_t
+#include <stdbool.h>
+#include <stdint.h>
+#include "cdPlayer.h"    // 提供 hmsf_t 等类型
 
-extern lv_obj_t *lb_driveModel
-extern lv_obj_t *lb_driveState
-extern lv_obj_t *lb_alBumTile
-extern lv_obj_t *lb_teackTitle
-extern lv_obj_t *lb_teackPerformer
-extern lv_obj_t *lb_preEmphasized
-extern lv_obj_t *lb_time
-extern lv_obj_t *lb_duretion
+/* ===== 全局控件指针（与 gui_cdPlayer.c 中的定义对应） ===== */
+extern lv_obj_t *area_topBar;
+extern lv_obj_t *area_bottomBar;
+extern lv_obj_t *area_player;
+extern lv_obj_t *area_left;
+extern lv_obj_t *area_right;
 
-extern lv_obj_t *bar playProgress
-extern lv_obj_t *bar meterleft
-extern lv_obj_t *bar meterRight
+extern lv_obj_t *lb_driveModel;
+extern lv_obj_t *lb_driveState;
+extern lv_obj_t *lb_albumTitle;
+extern lv_obj_t *lb_trackNumber;
+extern lv_obj_t *lb_trackTitle;
+extern lv_obj_t *lb_trackPerformer;
+extern lv_obj_t *lb_preEmphasized;
+extern lv_obj_t *lb_time;
+extern lv_obj_t *lb_duration;
 
-void gui_player_init();
+extern lv_obj_t *bar_playProgress;
+extern lv_obj_t *bar_meterLeft;
+extern lv_obj_t *bar_meterRight;
 
+extern lv_obj_t *chart_left;
+extern lv_chart_series_t *ser_left;
+extern lv_obj_t *chart_right;
+extern lv_chart_series_t *ser_right;
+
+extern lv_obj_t *lb_playState;
+extern lv_obj_t *lb_volume;
+
+/* ===== 对外接口 ===== */
+void gui_player_init(void);
 void gui_setDriveModel(const char *str);
 void gui_setDriveState(const char *str);
 void gui_setAlbumTitle(const char *str);
@@ -42,4 +53,8 @@ void gui_setVolume(int vol);
 void gui_setTrackNum(int current, int total);
 void gui_setMeter(int l, int r);
 
+#ifdef __cplusplus
+}
 #endif
+
+#endif /* GUI_CDPLAYER_H_ */
